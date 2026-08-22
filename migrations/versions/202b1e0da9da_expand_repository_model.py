566daf6f4e06370cad1696ca84fc9b62c6b863e6
unknown <saheedazeem4@gmail.com>
2026-08-22 22:38:55 +0100

    migrations

--- migrations/versions/202b1e0da9da_expand_repository_model.py
+++ migrations/versions/202b1e0da9da_expand_repository_model.py
@@ -11,7 +11,6 @@ from collections.abc import Sequence
 import sqlalchemy as sa
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
 revision: str = "202b1e0da9da"
 down_revision: str | Sequence[str] | None = "f7dfaced807e"
